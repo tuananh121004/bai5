d80ae7030c280a1f95b3f94abe0213c62dab1b12
Admin <Admin@LAPTOP-9URIGF82>
2024-01-19 11:03:21 +0700

    Add project files.

---
+++ bai5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SanPham sanPham = new SanPham(1, "LapTop", 1000, 2);
+            Console.
+        }
+    }
+}
---
+++ bai5/gfqg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5
+{
+    internal class gfqg
+    {
+        public int MaSP { get; set; }
+        public string TenSP  { get; set;}
+        public double  DonGia { get; set; }
+        public int  SoLuong  { get; set; }
+
+    }
+    public SanPham
+}
